4c2506034ff0280da3f72e615b5cd9ea540261af
Jari Ahtiainen <p12920@edu.sasky.fi>
2024-08-06 09:08:24 +0300

    Sivupohjat lisätty

---
+++ liikuntaryhma/src/view/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <title>liikuntaryhma - <?=$this->e($title)?></title>
+    <meta charset="UTF-8">    
+  </head>
+  <body>
+    <header>
+      <h1>liikuntaryhma</h1>
+    </header>
+    <section>
+      <?=$this->section('content')?>
+    </section>
+    <footer>
+      <hr>
+      <div>liikuntaryhma by Kurpitsa</div>
+    </footer>
+  </body>
+</html>
