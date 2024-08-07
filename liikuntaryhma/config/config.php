d4372a9f332fea9f2db57334f62d7cf37138bd94
Jari Ahtiainen <p12920@edu.sasky.fi>
2024-08-07 11:58:11 +0300

    sivuston tyylimääritykset

--- liikuntaryhma/config/config.php
+++ liikuntaryhma/config/config.php
@@ -19,5 +19,6 @@ $config = array(
   define("MODEL_DIR", PROJECT_ROOT . "src/model/");
   define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
   define("BASEURL", $config['urls']['baseUrl']);
+  
 
 ?>
--- liikuntaryhma/src/view/template.php
+++ liikuntaryhma/src/view/template.php
@@ -2,15 +2,19 @@
 <html lang="fi">
   <head>
     <title>liikuntaryhma - <?=$this->e($title)?></title>
-    <meta charset="UTF-8">    
+    <meta charset="UTF-8">
+    <link href="styles/styles.css" rel="stylesheet">
+        
   </head>
   <body>
     <header>
       <h1><a href="<?=BASEURL?>">Liikuntaryhmät</a></h1>
     </header>
+    
     <section>
       <?=$this->section('content')?>
     </section>
+    
     <footer>
       <hr>
       <div>Liikuntaryhmät by Kurpitsa</div>
