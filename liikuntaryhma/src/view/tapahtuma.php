e2da08381c92be6b739db7bb337d6acd791aa401
Jari Ahtiainen <p12920@edu.sasky.fi>
2024-08-08 16:35:05 +0300

    ilmoittaudu-nappi tapahtumasivulle

--- liikuntaryhma/src/view/tapahtuma.php
+++ liikuntaryhma/src/view/tapahtuma.php
@@ -10,4 +10,16 @@
 <div>Alkaa: <?=$start->format('j.n.Y G:i')?></div>
 <div>Kesto: <?=$tapahtuma['kesto']?></div>
 
+<?php
+  if ($loggeduser) {
+    if (!$ilmoittautuminen) {
+      echo "<div class='flexarea'><a href='ilmoittaudu?id=$tapahtuma[idtapahtuma]' class='button'>ILMOITTAUDU</a></div>";    }
+    } else {
+      echo "<div class='flexarea'>";
+      echo "<div>Olet ilmoittautunut tapahtumaan!</div>";
+      echo "<a href='peru?id=$tapahtuma[idtapahtuma]' class='button'>PERU ILMOITTAUTUMINEN</a>";
+      echo "</div>";
+    }
+  
+?>
 
