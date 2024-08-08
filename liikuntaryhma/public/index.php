c241ecafee25c7b1d516f5a6970343cf87b54cdf
Jari Ahtiainen <p12920@edu.sasky.fi>
2024-08-08 12:14:32 +0300

    istunnon suojaaminen

--- liikuntaryhma/public/index.php
+++ liikuntaryhma/public/index.php
@@ -58,6 +58,7 @@ error_reporting(E_ALL);
           if (isset($_POST['laheta'])) {
             require_once CONTROLLER_DIR . 'kirjaudu.php';
             if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
+               session_regenerate_id();
                $_SESSION['user'] = $_POST['email'];                           
                header("Location: " . $config['urls']['baseUrl']);             //header("Location: ") function sends user to appended address
             } else {
